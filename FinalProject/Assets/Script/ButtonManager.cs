74a8f5fb061105ffb9592b19a71dd3d4d2bfb67d
Jom-Winston <bjomwinston0107@gmail.com>
2022-11-27 15:40:19 +0800

    ButtonUpdateV1

--- FinalProject/Assets/Script/ButtonManager.cs
+++ FinalProject/Assets/Script/ButtonManager.cs
@@ -5,7 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-public void firstNextScene(){
+public void ModeScene(){
     SceneManager.LoadScene("Mode");
+}
+public void Option(){
+    SceneManager.LoadScene("Option");
+}
+public void MainScene(){
+    SceneManager.LoadScene("MainScene");
+}
+public void BeastFight(){
+    SceneManager.LoadScene("BeastFight");
+}
+public void RivalFight(){
+    SceneManager.LoadScene("RivalFight");
+}
+public void Quitapplication(){
+    Application.Quit();
 }
 }
